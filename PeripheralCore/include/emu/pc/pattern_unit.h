676152639b4753a9b1d10e86a2ee5ddf1373b993
Andrew Peck <anp@ucla.edu>
2014-03-20 12:57:47 -0700

    cleanup headers

--- PeripheralCore/include/emu/pc/pattern_unit.h
+++ PeripheralCore/include/emu/pc/pattern_unit.h
@@ -1,53 +1,27 @@
+//------------------------------------------------------------------------------
+//	CLCT Pattern Unit Emulator
+//------------------------------------------------------------------------------
 #ifndef PATTERN_UNIT_H
 #define PATTERN_UNIT_H
 //------------------------------------------------------------------------------
-//	CLCT Pattern Unit Emulator
-//
-//	Finds:	  Best matching pattern template and number of layers hit on that pattern for 1 key 1/2-strip
-//	Returns:  Best matching pattern template ID, and number of hits on the pattern
-//
-//	03/30/10 Initial translation from verilog
-//	09/07/10 Mod for vs2008
-//------------------------------------------------------------------------------
-// Headers
-//------------------------------------------------------------------------------
-	#include <stdio.h>
-	#include <iostream>
-	using namespace std;
-//------------------------------------------------------------------------------
-//	Debug print mode
-//------------------------------------------------------------------------------
-//	#define debug 1	// comment this line to turn off debug print
-
-	#ifdef debug
-	 #define dprintf fprintf
-	#else
-	 #define dprintf //
-	#endif
-    
-//------------------------------------------------------------------------------
-//	Prototypes
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-	void pattern_unit 
-	(
-// Inputs
-	int ly0[], 
-	int ly1[], 
-	int ly2[], 
-	int ly3[], 
-	int ly4[],
-	int ly5[],
-// Outputs
-	int	&pat_nhits,
-	int	&pat_id
-	);
+void pattern_unit 
+(
+ // Inputs
+ int ly0[], 
+ int ly1[], 
+ int ly2[], 
+ int ly3[], 
+ int ly4[],
+ int ly5[],
+ // Outputs
+ int	&pat_nhits,
+ int	&pat_id
+ );
 //------------------------------------------------------------------------------------------------------------------------
 // Function to sum number of layers hit
 // Returns 	count1s = (inp[5]+inp[4]+inp[3])+(inp[2]+inp[1]+inp[0]);
 //------------------------------------------------------------------------------------------------------------------------
-	int count1s(int pat[6]);
-//------------------------------------------------------------------------------------------
-// End pattern_unit
+int count1s(int pat[6]);
 //------------------------------------------------------------------------------------------
 #endif
+//------------------------------------------------------------------------------------------
--- PeripheralCore/include/emu/pc/service.h
+++ PeripheralCore/include/emu/pc/service.h
@@ -1,97 +1,43 @@
-#ifndef SERVICE_H
-#define SERVICE_H
 //------------------------------------------------------------------------------
 // Service routines for main
 //------------------------------------------------------------------------------
-#include <cstdlib>
+#ifndef SERVICE_H
+#define SERVICE_H
+//------------------------------------------------------------------------------
 #include <cstring> 
 #include <stdio.h>
 #include <iostream>
-#include <cmath>
-using namespace std; 
-
-//------------------------------------------------------------------------------
-//   Flip pattern ID numbers, because Im too lazy to flip the hs image
 //------------------------------------------------------------------------------
 int flip_pid(int pid); 
 //------------------------------------------------------------------------------
-//   Check data read vs data expected
+void ck(std::string msg_string, int data_read, int data_expect); 
 //------------------------------------------------------------------------------
-void ck(string msg_string, int data_read, int data_expect); 
+int cks(std::string msg_string, int data_read, int data_expect); 
 //------------------------------------------------------------------------------
-//   Check data read vs data expected, with status return
+void tok(std::string msg_string, double fdata_read, double fdata_expect, double tolerance, int &status); 
 //------------------------------------------------------------------------------
-int cks(string msg_string, int data_read, int data_expect); 
+int inquire(std::string test, std::string val, const int &minv, const int &maxv, const int &radix, int &now); 
 //------------------------------------------------------------------------------
-//   Check data read vs data expected, floating point version  with tolerance
+int inquir2(std::string test, std::string val, const int &minv, const int &maxv, const int &radix, int &num, int &now); 
 //------------------------------------------------------------------------------
-void tok(string msg_string, double fdata_read, double fdata_expect, double tolerance, int &status); 
+int inquirl(std::string test, std::string val, const long int &minv, const long int &maxv, const int &radix, long int &now); 
 //------------------------------------------------------------------------------
-//   Inquire prompt for integer
+void inquirb(std::string test, std::string val, bool &now); 
 //------------------------------------------------------------------------------
-int inquire(string test, string val, const int &minv, const int &maxv, const int &radix, int &now); 
-//------------------------------------------------------------------------------
-//   Inquire prompt for two integers
-//------------------------------------------------------------------------------
-int inquir2(string test, string val, const int &minv, const int &maxv, const int &radix, int &num, int &now); 
-//------------------------------------------------------------------------------
-//   Inquire prompt for long integer
-//------------------------------------------------------------------------------
-int inquirl(string test, string val, const long int &minv, const long int &maxv, const int &radix, long int &now); 
-//------------------------------------------------------------------------------
-//   Inquire prompt for bool
-//------------------------------------------------------------------------------
-void inquirb(string test, string val, bool &now); 
-//------------------------------------------------------------------------------
-//   Inquire prompt for string
-//------------------------------------------------------------------------------
-void inquirs(string test, string val, string &now); 
-//------------------------------------------------------------------------------
-//   Convert integer bit string to an array
+void inquirs(std::string test, std::string val, std::string &now); 
 //------------------------------------------------------------------------------
 void bit_to_array(const int &idata, int iarray[], const int &n); 
 //------------------------------------------------------------------------------------------
-// Logical OR returns the OR of all the array elements as a single integer
-//------------------------------------------------------------------------------------------
 int arrayOr(int array[32]); 
-//------------------------------------------------------------------------------------------
-// Array AND returns an array of the AND of two arrays
 //------------------------------------------------------------------------------------------
 int *arrayAnd(int array1[32], int array2[32]); 
 //------------------------------------------------------------------------------
-//	CRC-22 with 16-bit parallel loading data and synchronous reset
-//	Polynomial = x22+x1+1
+void crc22a (
+        long int	&din,		// 16 bit integer
+        long int	&crc,		// 22 bit integer
+        int			reset);
 //------------------------------------------------------------------------------
-
+void nextCRC22_D16(int din[16], int crc[22], int newcrc[22]);
 //------------------------------------------------------------------------------
-// Entry crc22a()	
-//------------------------------------------------------------------------------
-	void crc22a (
-	long int	&din,		// 16 bit integer
-	long int	&crc,		// 22 bit integer
-	int			reset);
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// File:  CRC22_D16.v
-// Date:  Tue Dec  3 00:44:19 2002
-//
-// Purpose: Verilog module containing a synthesizable CRC function
-//   * polynomial: (0 1 22)
-//   * data width: 16
-//   * convention: the first serial data bit is D[15]
-// Info: jand@easics.be (Jan Decaluwe)
-//	   http://www.easics.com
-//
-// 07/16/04 Ported to fortran
-// 08/26/08 Portet to c/cpp
-// 10/03/08 Move new crc array to caller
-// 10/05/08 Add explicit array dimensions to prototype
-//------------------------------------------------------------------------------
-	void nextCRC22_D16(int din[16], int crc[22], int newcrc[22]);
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-// The bitter end
-//------------------------------------------------------------------------------
-
 #endif
+//------------------------------------------------------------------------------
